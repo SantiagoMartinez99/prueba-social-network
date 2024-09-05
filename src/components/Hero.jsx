8eed24152a9e2841a0c033e0826547d1c4f5387a
David <sm307560@gmail.com>
2024-09-05 14:03:38 -0500

    fix: mobile hero view

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,34 +3,34 @@ import NewPostImg from "../assets/newpostImg.png";
 
 function Hero() {
   return (
-    <div className="hero animated-background bg-gradient-to-r from-indigo-400 to-cyan-400 min-h-screen pt-10 font-inter">
-      <div className="hero-content flex-col lg:flex-row-reverse">
-        <div className="bg-white relative translate-x-24">
+    <div className="hero animated-background bg-gradient-to-r from-indigo-400 to-cyan-400 min-h-screen pt-40 lg:pt-10 font-inter">
+      <div className="hero-content flex-col lg:flex-row-reverse justify-center items-center lg:items-start">
+        <div className="bg-white relative lg:translate-x-24 w-full lg:w-1/2 flex justify-center lg:block">
           <img
             src="https://i.pinimg.com/564x/a1/40/37/a14037d51cb82beea0799e775332e6d1.jpg"
-            className="max-w-sm h-auto  m-2 p-5 pb-10 mb-16"
+            className="max-w-xs sm:max-w-sm h-auto m-2 p-5 pb-10 mb-16 mx-auto lg:mx-0"
           />
           <img
             src={NewPostImg}
-            className="max-w-sm h-auto absolute m-2 p-5 pb-10 mb-10 -top-32 -right-20"
+            className="max-w-xs sm:max-w-sm h-auto absolute m-2 p-5 pb-10 mb-10 -top-32 right-0 lg:-right-20 mx-auto lg:mx-0"
           />
           <img
             src={NoteImg}
-            className="w-max-md  absolute -bottom-0 -right-6"
+            className="w-max-md absolute -bottom-0 right-0 lg:-right-6 mx-auto lg:mx-0"
           />
         </div>
-        <div>
-          <h1 className="text-7xl font-bold text-white">
+        <div className="text-center lg:text-left">
+          <h1 className="text-5xl lg:text-7xl font-bold text-white">
             Comparte los momentos más especiales con quienes más te importan.
           </h1>
-          <p className="text-3xl py-6 text-white">
+          <p className="text-2xl lg:text-3xl py-6 text-white">
             Captura y comparte tus recuerdos favoritos en nuestra red social de
             imágenes. Conéctate con amigos y familiares a través de fotos y
             comentarios. ¡Haz que cada instante cuente y mantén vivas tus
             conexiones!
           </p>
           <button className="btn btn-primary bg-pink-500 text-white text-xl border-none">
-            Unete ahora
+            Únete ahora
           </button>
         </div>
       </div>
