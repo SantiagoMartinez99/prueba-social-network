9d821d8a987ae565e43ece86164382c792aedd15
David <sm307560@gmail.com>
2024-09-05 16:34:25 -0500

    feat: ModalNewPost notification + Post upload and like queries

--- src/components/ModalNewPost.jsx
+++ src/components/ModalNewPost.jsx
@@ -6,6 +6,8 @@ import { db, storage } from "../firebase";
 import ImageFilter from "./ImageFilter";
 import { collection, addDoc } from "firebase/firestore";
 import { v4 as uuidv4 } from "uuid";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const dataURLToBlob = (dataURL) => {
   const [header, data] = dataURL.split(",");
@@ -71,9 +73,10 @@ function ModalNewPost({ isOpen, setIsOpen }) {
         uploadedAt: new Date(),
       });
 
-      console.log("File uploaded and post saved to Firestore!");
+      setIsOpen(false);
+      toast.success("Post creado con éxito");
     } catch (error) {
-      console.error("Error uploading file or saving post:", error);
+      toast.error("Error al crear el post");
     }
   };
   const handleUploadClick = () => {
@@ -90,57 +93,62 @@ function ModalNewPost({ isOpen, setIsOpen }) {
     }
   };
 
-  return isOpen ? (
+  return (
     <>
-      <div className="fixed inset-0 bg-black bg-opacity-50 z-50 flex items-center justify-center">
-        <div className="bg-white rounded-lg shadow-lg max-w-xl mx-auto p-6 relative">
-          <button
-            className="absolute top-2 right-2 text-gray-500 hover:text-gray-800"
-            onClick={() => setIsOpen(false)}
-          >
-            &times;
-          </button>
+      {isOpen ? (
+        <>
+          <div className="fixed inset-0 bg-black bg-opacity-50 z-50 flex items-center justify-center">
+            <div className="bg-white rounded-lg shadow-lg max-w-xl mx-auto p-6 relative">
+              <button
+                className="absolute top-2 right-2 text-gray-500 hover:text-gray-800"
+                onClick={() => setIsOpen(false)}
+              >
+                &times;
+              </button>
 
-          <h2 className="text-lg font-semibold mb-4">Subir imágenes</h2>
-          <input
-            type="file"
-            accept="image/*"
-            onChange={handleImageChange}
-            capture="environment"
-            className="mb-4"
-          />
-          {selectedImage && (
-            <div className="flex">
-              <div className="w-1/2">
-                <ImageFilter imageSrc={selectedImage} />
-              </div>
-
-              <div className="w-1/2 ml-4 bg-gray-100 p-4 rounded-lg shadow-md">
-                <h3 className="text-md font-semibold mb-2">Detalles</h3>
-
-                <div className="avatar items-center gap-4 my-2 py-2">
-                  <div className="w-10 rounded-full">
-                    <img src={user.photoURL} />
+              <h2 className="text-lg font-semibold mb-4">Subir imágenes</h2>
+              <input
+                type="file"
+                accept="image/*"
+                onChange={handleImageChange}
+                capture="environment"
+                className="mb-4"
+              />
+              {selectedImage && (
+                <div className="flex">
+                  <div className="w-1/2">
+                    <ImageFilter imageSrc={selectedImage} />
                   </div>
-                  <p className="text-xs font-bold">{user.displayName}</p>
+
+                  <div className="w-1/2 ml-4 bg-gray-100 p-4 rounded-lg shadow-md">
+                    <h3 className="text-md font-semibold mb-2">Detalles</h3>
+
+                    <div className="avatar items-center gap-4 my-2 py-2">
+                      <div className="w-10 rounded-full">
+                        <img src={user.photoURL} />
+                      </div>
+                      <p className="text-xs font-bold">{user.displayName}</p>
+                    </div>
+                    <textarea
+                      className="shadow-md w-full"
+                      placeholder="Ingresa una descripción"
+                    ></textarea>
+                    <button
+                      onClick={handleUploadClick}
+                      className="mt-4 bg-green-500 text-white px-4 py-2 rounded-lg absolute bottom-0 right-10 bottom-10"
+                    >
+                      Subir
+                    </button>
+                  </div>
                 </div>
-                <textarea
-                  className="shadow-md w-full"
-                  placeholder="Ingresa una descripción"
-                ></textarea>
-                <button
-                  onClick={handleUploadClick}
-                  className="mt-4 bg-green-500 text-white px-4 py-2 rounded-lg absolute bottom-0 right-10 bottom-10"
-                >
-                  Subir
-                </button>
-              </div>
+              )}
             </div>
-          )}
-        </div>
-      </div>
+          </div>
+        </>
+      ) : null}
+      <ToastContainer />;
     </>
-  ) : null;
+  );
 }
 
 export default ModalNewPost;
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -5,16 +5,18 @@ import {
   doc,
   updateDoc,
   arrayUnion,
+  arrayRemove,
 } from "firebase/firestore";
 import { db } from "../firebase";
 import { useAuth } from "../context/AuthContext";
 import { HeartIcon as HeartOutline } from "@heroicons/react/24/outline";
 import { HeartIcon as HeartSolid } from "@heroicons/react/24/solid";
 
-function PostsList() {
+function Posts() {
   const [posts, setPosts] = useState([]);
   const [commentText, setCommentText] = useState("");
   const { user } = useAuth();
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -22,6 +24,7 @@ function PostsList() {
       const postsArray = querySnapshot.docs.map((doc) => ({
         id: doc.id,
         ...doc.data(),
+        likes: doc.data().likes || [],
       }));
       setPosts(postsArray);
     };
@@ -29,11 +32,64 @@ function PostsList() {
     fetchPosts();
   }, []);
 
-  const [isLiked, setIsLiked] = useState(false);
-  const [likeCount, setLikeCount] = useState(123);
-  const handleLike = () => {
-    setIsLiked(!isLiked);
-    setLikeCount(isLiked ? likeCount - 1 : likeCount + 1);
+  const addLikeToPost = async (postId, userId) => {
+    try {
+      const postRef = doc(db, "posts", postId);
+
+      // Actualiza el campo 'likes' añadiendo el ID del usuario
+      await updateDoc(postRef, {
+        likes: arrayUnion(userId),
+      });
+      console.log("Like added successfully!");
+    } catch (error) {
+      console.error("Error adding like:", error);
+    }
+  };
+
+  const removeLikeFromPost = async (postId, userId) => {
+    try {
+      const postRef = doc(db, "posts", postId);
+
+      // Actualiza el campo 'likes' eliminando el ID del usuario
+      await updateDoc(postRef, {
+        likes: arrayRemove(userId),
+      });
+      console.log("Like removed successfully!");
+    } catch (error) {
+      console.error("Error removing like:", error);
+    }
+  };
+
+  const handleLikeClick = async (postId, hasLiked) => {
+    if (loading) return;
+
+    setLoading(true);
+    if (hasLiked) {
+      await removeLikeFromPost(postId, user.uid);
+      setPosts((prevPosts) =>
+        prevPosts.map((post) =>
+          post.id === postId
+            ? {
+                ...post,
+                likes: post.likes.filter((uid) => uid !== user.uid),
+              }
+            : post
+        )
+      );
+    } else {
+      await addLikeToPost(postId, user.uid);
+      setPosts((prevPosts) =>
+        prevPosts.map((post) =>
+          post.id === postId
+            ? {
+                ...post,
+                likes: [...post.likes, user.uid],
+              }
+            : post
+        )
+      );
+    }
+    setLoading(false);
   };
 
   const handleAddComment = async (postId) => {
@@ -49,97 +105,121 @@ function PostsList() {
     });
 
     setCommentText("");
+    setPosts((prevPosts) =>
+      prevPosts.map((post) =>
+        post.id === postId
+          ? {
+              ...post,
+              comments: [
+                ...post.comments,
+                {
+                  userId: user.uid,
+                  userName: user.displayName,
+                  comment: commentText,
+                  createdAt: new Date(),
+                },
+              ],
+            }
+          : post
+      )
+    );
   };
 
   return (
     <div>
-      {posts.map((post, index) => (
-        <div
-          key={index}
-          className="card max-w-md mx-auto bg-base-100 shadow-xl my-40"
-        >
-          <div className="card-body p-4">
-            <div className="flex items-center gap-4">
-              <div className="avatar">
-                <div className="w-10 h-10 rounded-full">
-                  <img
-                    src={
-                      post.userPhotoURL || "/placeholder.svg?height=40&width=40"
-                    }
-                    alt={post.userName}
-                  />
+      {posts.map((post) => {
+        const hasLiked =
+          Array.isArray(post.likes) && post.likes.includes(user.uid);
+
+        return (
+          <div
+            key={post.id}
+            className="card max-w-md mx-auto bg-base-100 shadow-xl my-40"
+          >
+            <div className="card-body p-4">
+              <div className="flex items-center gap-4">
+                <div className="avatar">
+                  <div className="w-10 h-10 rounded-full">
+                    <img
+                      src={
+                        post.userPhotoURL ||
+                        "/placeholder.svg?height=40&width=40"
+                      }
+                      alt={post.userName}
+                    />
+                  </div>
+                </div>
+                <div className="flex flex-col">
+                  <p className="text-sm font-semibold">
+                    {post.userName || "usuario"}
+                  </p>
                 </div>
               </div>
-              <div className="flex flex-col">
-                <p className="text-sm font-semibold">
-                  {post.userName || "usuario"}
-                </p>
+            </div>
+            <figure className="px-0 pt-0">
+              <img
+                src={post.imageURL}
+                alt="Imagen de la publicación"
+                className="w-full h-auto"
+              />
+            </figure>
+            <div className="card-body p-4 pt-2">
+              <div className="flex justify-between items-center w-full">
+                <div className="flex gap-4">
+                  <button
+                    className="btn btn-ghost btn-square btn-sm group"
+                    onClick={() => handleLikeClick(post.id, hasLiked)}
+                    aria-label={hasLiked ? "Quitar me gusta" : "Me gusta"}
+                  >
+                    {hasLiked ? (
+                      <HeartSolid className="h-6 w-6 text-red-500 transform group-active:scale-125 transition-transform duration-200" />
+                    ) : (
+                      <HeartOutline className="h-6 w-6 group-hover:text-red-500 transform group-active:scale-125 transition-transform duration-200" />
+                    )}
+                  </button>
+                </div>
+                <p className="font-semibold ">{post.likes.length} Me gusta</p>
+              </div>
+              <div className="text-sm space-y-1">
+                <span className="font-semibold">{post.userName}:</span>
+                {post.description}
+                <h4 className="text-gray-600 text-sm ">Comentarios</h4>
+                {post.comments && post.comments.length > 0 ? (
+                  post.comments.map((comment, idx) => (
+                    <div key={idx} className="mt-2 text-sm">
+                      <span className="font-semibold">
+                        {comment.userName}:{" "}
+                      </span>
+                      <span>{comment.comment}</span>
+                    </div>
+                  ))
+                ) : (
+                  <p className="text-xs text-gray-500">
+                    No hay comentarios todavía.
+                  </p>
+                )}
+                <div className="mt-4">
+                  <input
+                    type="text"
+                    placeholder="Escribe un comentario..."
+                    value={commentText}
+                    onChange={(e) => setCommentText(e.target.value)}
+                    className="input input-bordered w-full mb-4 h-9"
+                  />
+                  <button
+                    onClick={() => handleAddComment(post.id)}
+                    className="btn btn-primary btn-sm text-sm font-bold text-white bg-gradient-to-r from-indigo-400 to-cyan-400 border-none"
+                  >
+                    Comentar
+                  </button>
+                </div>
               </div>
             </div>
           </div>
-          <figure className="px-0 pt-0">
-            <img
-              src={post.imageURL}
-              alt="Imagen de la publicación"
-              className="w-full h-auto"
-            />
-          </figure>
-          <div className="card-body p-4 pt-2">
-            <div className="flex justify-between items-center w-full">
-              <div className="flex gap-4">
-                <button
-                  className="btn btn-ghost btn-square btn-sm group"
-                  onClick={handleLike}
-                  aria-label={isLiked ? "Quitar me gusta" : "Me gusta"}
-                >
-                  {isLiked ? (
-                    <HeartSolid className="h-6 w-6 text-red-500 transform group-active:scale-125 transition-transform duration-200" />
-                  ) : (
-                    <HeartOutline className="h-6 w-6 group-hover:text-red-500 transform group-active:scale-125 transition-transform duration-200" />
-                  )}
-                </button>
-              </div>
-              <p className="font-semibold ">{post.likes} Me gusta</p>
-            </div>
-            <div className="text-sm space-y-1">
-              <span className="font-semibold">{post.userName}:</span>
-              {post.description}
-              <h4 className="text-gray-600 text-sm ">Comentarios</h4>
-              {post.comments && post.comments.length > 0 ? (
-                post.comments.map((comment, idx) => (
-                  <div key={idx} className="mt-2 text-sm">
-                    <span className="font-semibold">{comment.userName}: </span>
-                    <span>{comment.comment}</span>
-                  </div>
-                ))
-              ) : (
-                <p className="text-xs text-gray-500">
-                  No hay comentarios todavía.
-                </p>
-              )}
-              <div className="mt-4">
-                <input
-                  type="text"
-                  placeholder="Escribe un comentario..."
-                  value={commentText}
-                  onChange={(e) => setCommentText(e.target.value)}
-                  className="input input-bordered w-full mb-4 h-9"
-                />
-                <button
-                  onClick={() => handleAddComment(post.id)}
-                  className="btn btn-primary btn-sm text-sm font-bold text-white bg-gradient-to-r from-indigo-400 to-cyan-400 border-none"
-                >
-                  Comentar
-                </button>
-              </div>
-            </div>
-
-            <div className="comments-section"></div>
-          </div>
-        </div>
-      ))}
+        );
+      })}
     </div>
   );
 }
 
-export default PostsList;
+export default Posts;
